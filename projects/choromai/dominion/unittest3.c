e6c650ee281cdafe6aef86e329a8d4302cd698e6
U-IKARIA-RZR\cgcha <i@ikaria.tv>
2018-07-21 11:59:36 -0700

    unit test 3 refactored, new case added

--- projects/choromai/dominion/unittest3.c
+++ projects/choromai/dominion/unittest3.c
@@ -2,57 +2,31 @@
 #include <stdio.h>
 #include "rngs.h"
 #include <stdlib.h>
-#include "race.h"
+#include "utils.h"
 
 //TESTING: handCard()
 
 //Test retriving first card, expected: Village
 int getFirstCard_village(int handPos, struct gameState *state)
 {
-  int pass = 0;
-
-  //setup state
-
   int card = handCard(handPos, state);
-
-  if (card == village)
-  {
-      pass = 1;
-  }
-
-  printf ("Test 1: Get card in position: 1 | Expection: Village | Result: ");
-  if(pass)
-    printf ("PASSED\n");
-  else
-    printf ("FAILED\n");
-
-  return pass;
+  return AssertCondition("Incorrect card at pos 0", card == village);
 }
 
 
 //Test retriving last card, expected: Smithy 
 int getLastCard_smithy(int handPos, struct gameState *state)
 {
-  int pass = 0;
-
-  //setup state
-
   int card = handCard(handPos, state);
-
-  if (card == smithy)
-  {
-      pass = 1;
-  }
-
-  printf ("Test 2: Get card in position: 5 | Expection: Smithy | Result: ");
-  if(pass)
-    printf ("PASSED\n");
-  else
-    printf ("FAILED\n");
-
-  return pass;
+  return AssertCondition("Incorrect card at pos 4", card == smithy);
 }
 
+//Test attempt to get card at position larger than hand size
+int getOutOfRange_None(int handPos, struct gameState *state)
+{
+  int card = handCard(handPos, state);
+  return AssertCondition("Out of range card has value, but shouldn't", card < 0 || card > 26);
+}
 
 int main (int argc, char** argv) {
   struct gameState G;
@@ -61,21 +35,30 @@ int main (int argc, char** argv) {
 
   printf("\n**************  handCard() ***********\n");
 
-  int success = 0;
+  int success = 1;
 
   initializeGame(2, k, 2, &G);
   int player = whoseTurn(&G);
 
+  //setup
   G.hand[player][0] = village;
   G.hand[player][1] = copper;
   G.hand[player][2] = copper;
   G.hand[player][3] = copper;
   G.hand[player][4] = smithy;
 
-  success += getFirstCard_village(0,&G);
-  success += getLastCard_smithy(4,&G);
+  G.handCount->player = 5;
 
-  if(success == 2)
+  success &= AssertTest("Test 1: Get card in position: 1 | Expected: Village",
+    getFirstCard_village(0,&G));
+
+  success &= AssertTest("Test 2: Get card in position: 5 | Expected: Smithy",
+   getLastCard_smithy(4,&G));
+
+  success &= AssertTest("Test 3: Try card out of range | Expected: invalid enum",
+   getOutOfRange_None(7,&G));
+
+  if(success)
   {
     printf ("ALL TESTS PASSED\n");
   }
